aec7bf5b852e2f08cf4108ab15ed99bde9bdb813
joao-zanutto <jpberno@gmail.com>
2024-03-06 05:34:04 -0800

    change home to /

--- src/components/common/navBar.jsx
+++ src/components/common/navBar.jsx
@@ -19,7 +19,7 @@ const NavBar = (props) => {
 										: "nav-item"
 								}
 							>
-								<Link to="/home">Home</Link>
+								<Link to="/">Home</Link>
 							</li>
 							<li
 								className={
