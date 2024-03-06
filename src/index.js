f7ef626e0784f68e639143ae069b9396a6eefcda
joao-zanutto <jpberno@gmail.com>
2024-03-06 06:06:54 -0800

    use create hash router

--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter, RouterProvider } from "react-router-dom";
+
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  } 
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<HashRouter basename={process.env.PUBLIC_URL}>
+		<RouterProvider router={router} />
+		{/* <HashRouter basename={process.env.PUBLIC_URL}>
 			<App />
-		</HashRouter>
+		</HashRouter> */}
 	</React.StrictMode>
 );
 
