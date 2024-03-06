a62442bfbca0661fe78d2f54d6a0f379c2e936da
joao-zanutto <jpberno@gmail.com>
2024-03-06 06:07:36 -0800

    include create hash router in import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, HashRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, HashRouter, RouterProvider, createHashRouter } from "react-router-dom";
 
 const router = createHashRouter([
   {
