bcec8e3f8208f2352b75d07dde7edd2c031c723e
joao-zanutto <jpberno@gmail.com>
2024-03-13 12:27:45 -0700

    add first project to portfolio

--- src/data/user.js
+++ src/data/user.js
@@ -38,49 +38,26 @@ const INFO = {
 
 	projects: [
 		{
-			title: "Project 1",
+			title: "terraform-aws-ecs-cluster",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+				"Terraform module used to deploy an ECS Cluster using EC2 instances as capacity provider.",
+			logo: [
+					"./tf.png",
+					"./aws.png"
+				  ],
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://github.com/joao-zanutto/terraform-aws-ecs-cluster",
 		},
 
 		{
 			title: "Project 2",
 			description:
 				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			logo: ["https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png"],
 			linkText: "View Project",
 			link: "https://github.com",
 		},
 
-		{
-			title: "Project 3",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
-
-		{
-			title: "Project 4",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
-
-		{
-			title: "Project 5",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
 	],
 };
 
